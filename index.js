c0655f43aab1db255a45f740feec917557a35be6
Tupay Felkin <61619620+RubyFalcon@users.noreply.github.com>
2025-01-08 15:44:07 +0000

    create initial logic for pplaying a round

--- index.js
+++ index.js
@@ -1,22 +1,130 @@
-function createGameBoard(row, col) {
-    let board = [];
-    for (let i = 0 ; i < row; i++){
-        let boardRow = [];
-        for (let j = 0; j < col; j++) {
-            boardRow.push(" ");
 
-        }
-        board.push(boardRow);
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+  
+    // Create a 2d array that will represent the state of the game board
+    // For this 2d array, row 0 will represent the top row and
+    // column 0 will represent the left-most column.
+    // This nested-loop technique is a simple and common way to create a 2d array.
+    for (let i = 0; i < rows; i++) {
+      board[i] = [];
+      for (let j = 0; j < columns; j++) {
+        board[i].push(Cell());
+      }
     }
-    return board;
-}
-
-function createGame() {
-    const gameBoard = createGameBoard(3,3);
+  
+    // This will be the method of getting the entire board that our
+    // UI will eventually need to render it.
+    const getBoard = () => board;
+  
+    // In order to drop a piece, we need to find what the lowest point of the
+    // selected column is, *then* change that cell's value to the player number
+    const playPiece = (row, column, player) => {
+      // Our board's outermost array represents the row,
+      // so we need to loop through the rows, starting at row 0,
+      // find all the rows that don't have a piece, then take the
+      // last one, which will represent the bottom-most empty cell
+    
+      board[row][column].addPiece(player);
+    };
+  
+    // This method will be used to print our board to the console.
+    // It is helpful to see what the board looks like after each turn as we play,
+    // but we won't need it after we build our UI
+    const printBoard = () => {
+      const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+      console.log(boardWithCellValues);
+    };
+  
+    // Here, we provide an interface for the rest of our
+    // application to interact with the board
+    return { getBoard, playPiece, printBoard };
+  }
+  
+  /*
+  ** A Cell represents one "square" on the board and can have one of
+  ** 0: no piece is in the square,
+  ** 1: Player One's piece,
+  ** 2: Player 2's piece
+  */
+  
+  function Cell() {
+    let value = " ";
+  
+    // Accept a player's piece to change the value of the cell
+    const addPiece = (player) => {
+      value = player;
+    };
+  
+    // How we will retrieve the current value of this cell through closure
+    const getValue = () => value;
+  
     return {
-        gameBoard,
-    }
-}
-
-
-console.table(gameBoard)
+      addPiece,
+      getValue
+    };
+  }
+  
+  /* 
+  ** The GameController will be responsible for controlling the 
+  ** flow and state of the game's turns, as well as whether
+  ** anybody has won the game
+  */
+  function GameController(
+    playerOneName = "Player One",
+    playerTwoName = "Player Two"
+  ) {
+    const board = Gameboard();
+  
+    const players = [
+      {
+        name: playerOneName,
+        piece: "X"
+      },
+      {
+        name: playerTwoName,
+        piece: "O"
+      }
+    ];
+  
+    let activePlayer = players[0];
+  
+    const switchPlayerTurn = () => {
+      activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+    const getActivePlayer = () => activePlayer;
+  
+    const printNewRound = () => {
+      board.printBoard();
+      console.log(`${getActivePlayer().name}'s turn.`);
+    };
+  
+    const playRound = (row, column) => {
+      // Drop a piece for the current player
+      console.log(
+        ` ${getActivePlayer().name} plays ${getActivePlayer().piece} into column ${column} and row ${row}`
+      );
+      board.playPiece(row, column, getActivePlayer().piece);
+  
+      /*  This is where we would check for a winner and handle that logic,
+          such as a win message. */
+  
+      // Switch player turn
+      switchPlayerTurn();
+      printNewRound();
+    };
+  
+    // Initial play game message
+    printNewRound();
+  
+    // For the console version, we will only use playRound, but we will need
+    // getActivePlayer for the UI version, so I'm revealing it now
+    return {
+      playRound,
+      getActivePlayer
+    };
+  }
+  
+  const game = GameController();
